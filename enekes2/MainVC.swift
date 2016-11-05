afc625139d6a0726917563ba960ae05cd1d250b8
Gabriel Erdelyi <erdgabios@gmail.com>
2016-11-05 19:49:32 +0100

    UI part 1

--- enekes2/MainVC.swift
+++ enekes2/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  enekes2
 //
 //  Created by G on 05/11/2016.
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
